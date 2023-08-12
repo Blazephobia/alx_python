95ccc5020565bd544422d0847c6c594b058e00e1
Blazephobia <137614489+Blazephobia@users.noreply.github.com>
2023-08-12 00:16:21 -0400

    functions

---
+++ python-functions/4-fibonacci.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def fibonacci_sequence(n)
+    if n<= 0:
+        return []
+    elif n==1:
+        return [0]
+    fibonacci =[0, 1]
+    for i in range(2, n)
+    fibonacci_append(fibonacci[i - 1]+ fibonacci[i - 2])
+    return fibonacci
+
+
+    
+    
