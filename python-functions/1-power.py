80ea8e05d9fa9acc760a28863aa449211aee9d3b
Blazephobia <137614489+Blazephobia@users.noreply.github.com>
2023-08-11 23:34:30 -0400

    function

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,19 +1,5 @@
 #!/usr/bin/python3
 
 def pow(a, b):
-    result = 1
-    if b < 0:
-        a = 1 / a
-        b = -b
-    while b > 0:
-        if b % 2 == 1:
-            result *= a
-        a *= a
-        b //= 2
-    return result
-
-print(pow(2, 2))
-print(pow(98, 2))
-print(pow(98, 0))
-print(pow(100, -2))
-print(pow(-4, 5))
+    result a ** b
+    
