6ea767671240c7c5ffbe20317472726b6d49afd8
Blazephobia <137614489+Blazephobia@users.noreply.github.com>
2023-08-17 04:47:20 -0400

    Functions

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
-def fibonacci_sequence(n)
+def fibonacci_sequence(n):
     if n<= 0:
         return []
     elif n==1:
         return [0]
-    fibonacci =[0, 1]
-    for i in range(2, n)
-    fibonacci_append(fibonacci[i - 1]+ fibonacci[i - 2])
-    return fibonacci
+    elif n==2:
+        return [0, 1]
+    
+    sequence =[0, 1]
+    for i in range(2, n):
+        next_fib = sequence[i - 1] + sequence[i - 2]
+        sequence.append(next_fib)
+        
+    return sequence
 
 
     
--- python-functions/6-password.py
+++ python-functions/6-password.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 def validate_password(password):
-    if len(password)
-    return False
+    if len(password) < 8:
+        return False
+    
 has_upper = False
 has_lower = False
 has_digit = False
+has_space = False
+
 for char in password:
     if char.isupper():
         has_upper = True
@@ -13,5 +16,7 @@ for char in password:
     if char.isdigit():
         has_digit = True
     if char.isspace():
-        has_digit = False
-return has_upper and has_lower and has_digit
+        has_digit = True
+        
+        return has_upper and has_lower and has_digit and not has_space
+    
