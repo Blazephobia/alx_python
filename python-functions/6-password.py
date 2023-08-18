b06257d487d1c2504c53754efbf68818d50ac2b6
Blazephobia <137614489+Blazephobia@users.noreply.github.com>
2023-08-17 21:12:47 -0400

    function

--- python-functions/6-password.py
+++ python-functions/6-password.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
 def validate_password(password):
+
     if len(password) < 8:
         return False
     
-has_upper = False
-has_lower = False
-has_digit = False
-
-for char in password:
-    if char.isupper():
-        has_upper = True
-    elif char.islower():
-        has_lower = True
-    elif char.isdigit():
-        has_digit = True
-    elif char == ' ':
-        return False
+    has_uppercase = False
+    has_lowercase = False
+    has_digit = False
     
-return has_upper and has_lower and has_digit
+    for char in password:
+        if char.isupper():
+            has_uppercase = True
+        elif char.islower():
+            has_lowercase = True
+        elif char.isdigit():
+            has_digit = True
+        elif char.isspace():
+            return False
     
+    return has_uppercase and has_lowercase and has_digit
