4bbc5627b1f6c3216ef0fdb9f7c0e73d577d188d
Blazephobia <137614489+Blazephobia@users.noreply.github.com>
2023-08-27 02:22:07 -0400

    data

--- python-data_structures/2-multiple_returns.py
+++ python-data_structures/2-multiple_returns.py
@@ -3,12 +3,3 @@ def multiple_returns(sentence):
         return (0, None)
     else:
         return (len(sentence), sentence[0])
-
-# Example usage:
-sentence = "Hello, world!"
-result = multiple_returns(sentence)
-print(result)  # Output will be: (13, 'H')
-
-empty_sentence = ""
-empty_result = multiple_returns(empty_sentence)
-print(empty_result)  # Output will be: (0, None)
