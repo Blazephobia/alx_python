938b71f6ac8bf70824c396f81255e2eaf06f1fbb
Blazephobia <137614489+Blazephobia@users.noreply.github.com>
2023-08-27 02:28:46 -0400

    data

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,7 +1,7 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for num in row:
-            # Use str.format() to format and print each integer
-            print("{:d}".format(num), end=" ")
+        for index, num in enumerate(row):
+            if index > 0:
+                print(" ", end="")
+            print("{:d}".format(num), end="")
         print()
-        
