df5ee7dbbdaabc09142f1a439623ee6e49f1c94e
Blazephobia <137614489+Blazephobia@users.noreply.github.com>
2023-08-27 04:03:37 -0400

    classes

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,9 +1,19 @@
+#!/usr/bin/python3
+"""Square class definition"""
+
+
 class Square:
-    def __init__(self, size=0):
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size  # Private instance attribute
-except (TypeError, ValueError) as e:
-    print(e)
+    """Represents a square
+
+    Attributes:
+        __size (int): size of a side of the square
+    """
+    def __init__(self, size):
+        """Initializes a square
+
+        Args:
+            size (int): size of a side of the square
+
+        Returns: None
+        """
+        self.__size = size
