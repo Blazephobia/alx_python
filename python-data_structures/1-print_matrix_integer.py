efaa8f54a8dd362c344bf9dfee96f3056f4362fc
Blazephobia <137614489+Blazephobia@users.noreply.github.com>
2023-08-27 02:24:01 -0400

    data

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -4,12 +4,3 @@ def print_matrix_integer(matrix=[[]]):
             # Use str.format() to format and print each integer
             print("{:d}".format(num), end=" ")
         print()  # Move to the next line after printing a row
-
-# Example usage:
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-print_matrix_integer(matrix)
