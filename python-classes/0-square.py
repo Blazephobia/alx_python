2475566a71a3d0f772fad87d78456ceb34557f89
Blazephobia <137614489+Blazephobia@users.noreply.github.com>
2023-08-27 03:59:11 -0400

    classes

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,7 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
 class Square:
-    def __init__(self, size):
-        self.__size = size 
-print(square1.__dict__)  
-print(square2.__dict__)
+    """An empty Square class"""
+    pass
