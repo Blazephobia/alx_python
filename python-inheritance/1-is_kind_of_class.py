8025ee121e462829734e86357e92b6f37642ff96
Blazephobia <137614489+Blazephobia@users.noreply.github.com>
2023-08-27 04:48:43 -0400

    i

--- python-inheritance/1-is_kind_of_class.py
+++ python-inheritance/1-is_kind_of_class.py
@@ -1,26 +1,9 @@
 #!/usr/bin/python3
+"""
+Contains the is_kind_of_class function
+"""
+
+
 def is_kind_of_class(obj, a_class):
-    return isinstance(obj, a_class)
-
-# Example usage:
-class Vehicle:
-    pass
-
-class Car(Vehicle):
-    pass
-
-class Bicycle(Vehicle):
-    pass
-
-car_obj = Car()
-bicycle_obj = Bicycle()
-vehicle_obj = Vehicle()
-string = "Hello"
-
-print(is_kind_of_class(car_obj, Car))  # Output: True
-print(is_kind_of_class(car_obj, Vehicle))  # Output: True
-print(is_kind_of_class(bicycle_obj, Bicycle))  # Output: True
-print(is_kind_of_class(bicycle_obj, Vehicle))  # Output: True
-print(is_kind_of_class(vehicle_obj, Vehicle))  # Output: True
-print(is_kind_of_class(string, str))  # Output: True
-print(is_kind_of_class(car_obj, str))  # Output: False
+    """True if obj is an instance or inherited from a_class, else False"""
+    return (isinstance(obj, a_class))
