572ddf7afae14d7885ac2cdd28247941f597ab48
Blazephobia <137614489+Blazephobia@users.noreply.github.com>
2023-08-27 04:58:05 -0400

    i

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-"""
-Contains the class BaseGeometry and subclass Rectangle
-"""
+class BaseGeometry:
+    def area(self):
+        raise Exception("area() is not implemented")
 
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
-
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
 
 class Rectangle(BaseGeometry):
-    """A representation of a rectangle"""
     def __init__(self, width, height):
-        """instantiation of the rectangle"""
         self.integer_validator("width", width)
-        self.__width = width
         self.integer_validator("height", height)
+        self.__width = width
         self.__height = height
