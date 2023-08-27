906b41ec24caa4c9747585ab35b6e866d305d744
Blazephobia <137614489+Blazephobia@users.noreply.github.com>
2023-08-27 04:13:17 -0400

    classes

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,56 +1,46 @@
-#!/usr/bin/python3
-"""Defines a class Square"""
-
-
 class Square:
-    """Represents a square
-
-    Attributes:
-        __size (int): size of a side of the square
-    """
     def __init__(self, size=0):
-        """initializes the square
-
-        Args:
-            size (int): size of a side of the square
-
-        Returns:
-            None
-        """
-        self.size = size
-
-    def area(self):
-        """calculates the square's area
-
-        Returns:
-            The area of the square
-        """
-        return (self.__size) ** 2
-
+        self.size = size  # Use the property setter
+        
     @property
     def size(self):
-        """getter of __size
-
-        Returns:
-            The size of the square
-        """
         return self.__size
-
+    
     @size.setter
     def size(self, value):
-        """setter of __size
-
-        Args:
-            value (int): the size of a size of the square
-
-        Returns:
-            None
-        """
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+        
+    def area(self):
+        return self.__size ** 2
+    
+    def my_print(self):
+        if self.__size == 0:
+            print()
         else:
-            if value < 0:
-                raise ValueError("size must be >= 0")
-            else:
-                self.__size = value
-                
+            for _ in range(self.__size):
+                print("#" * self.__size)
+
+# Example usage:
+try:
+    square1 = Square(5)
+    square1.my_print()  # Output: #####
+                        #         #####
+                        #         #####
+                        #         #####
+                        #         #####
+    
+    square2 = Square(3)
+    square2.my_print()  # Output: ###
+                        #         ###
+                        #         ###
+    
+    square3 = Square(0)
+    square3.my_print()  # Output: (empty line)
+    
+    square4 = Square(-2)  # This will raise a ValueError
+except (TypeError, ValueError) as e:
+    print(e)
