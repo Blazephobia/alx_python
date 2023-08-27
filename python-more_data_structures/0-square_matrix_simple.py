9487f2578a833395f305173020b5bd81a52fcdc8
Blazephobia <137614489+Blazephobia@users.noreply.github.com>
2023-08-27 03:26:25 -0400

    data

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,2 +1,5 @@
 def square_matrix_simple(matrix=[]):
     return [[num ** 2 for num in row] for row in matrix]
+squared_matrix = square_matrix_simple(original_matrix)
+for row in squared_matrix:
+    print(row)
